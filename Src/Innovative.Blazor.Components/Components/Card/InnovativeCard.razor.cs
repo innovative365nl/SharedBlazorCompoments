8e76f035d6b6205fe3ba62c163457dc8a1906b01
Vincent Boots <vincent.boots@innovative.nl>
2025-04-03 19:29:42 +0200

    feat: Add XML documentation for parameters in InnovativeCard component

--- Src/Innovative.Blazor.Components/Components/Card/InnovativeCard.razor.cs
+++ Src/Innovative.Blazor.Components/Components/Card/InnovativeCard.razor.cs
@@ -1,5 +1,5 @@
 #region
-
+    
 using Microsoft.AspNetCore.Components;
 
 #endregion
@@ -8,9 +8,18 @@ namespace Innovative.Blazor.Components.Components.Card;
 
 public partial class InnovativeCard
 {
+    /// <summary>
+    /// Indicates whether the card is in a loading state. When true, displays a loading indicator.
+    /// </summary>
     [Parameter] public bool IsLoading { get; set; }
 
+    /// <summary>
+    /// The title text displayed at the top of the card.
+    /// </summary>
     [Parameter] public string? Title { get; set; }
 
+    /// <summary>
+    /// The content to be displayed inside the card. Required parameter.
+    /// </summary>
     [Parameter] public required RenderFragment ChildContent { get; set; }
 }
