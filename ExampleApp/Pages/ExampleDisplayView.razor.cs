b123e61a7150245e1443e0327113d7b68d883e37
Vincent Boots <vincent.boots@innovative.nl>
2025-04-03 16:32:14 +0200

    feat: Add ExampleDisplayView component with SimplePersonModel for dynamic display

---
+++ ExampleApp/Pages/ExampleDisplayView.razor.cs
@@ -0,0 +1,55 @@
+using ExampleApp.Extensions;
+using ExampleApp.Translations;
+using Innovative.Blazor.Components.Components.Dialog;
+using Microsoft.AspNetCore.Components;
+
+namespace ExampleApp.Pages;
+
+public partial class ExampleDisplayView : ComponentBase
+{
+    public ExampleDisplayView()
+    {
+        _person = new SimplePersonModel()
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            IsActive = true,
+            BirthDate = new DateTime(1993,
+                5,
+                12),
+        };
+    }
+
+    private readonly SimplePersonModel _person;
+}
+
+[UIFormClass(title: nameof(Example.DialogService_Person),
+    ResourceType = typeof(Example), ColumnOrder = new[] { "Name", "EmployeeInfo", "Description" },
+    ColumnWidthNames = new[] { "Name", "EmployeeInfo", "Description" },
+    ColumnWidthValues = new[] { 1, 1, 3 })]
+public class SimplePersonModel
+{
+    [UIFormField(name: "First Name", ColumnGroup = "Name")]
+    public string? FirstName { get; set; }
+
+    [UIFormField(name: "Last Name", ColumnGroup = "Name")]
+    public string? LastName { get; set; }
+
+    // [UIFormField(name: "Age", ColumnGroup = "EmployeeInfo")]
+    //public int? Age => DateTime.Now.Year - BirthDate!.Value.Year;
+
+    [UIFormField(name: "Is Active", ViewComponent = typeof(CustomBoolStyle), ColumnGroup = "EmployeeInfo")]
+    public bool IsActive { get; set; }
+
+    [UIFormField(name: "Birth Date", ColumnGroup = "EmployeeInfo")]
+    public DateTime? BirthDate { get; set; }
+
+    [UIFormField(name: "Description", UseWysiwyg = true, ColumnGroup = "Description")]
+    public string? Description { get; set; }
+//
+//     [UIFormViewAction(Name = "Update Password", Order = 1)]
+//     public  Action<int>? UpdatePasswordAction { get; set; }
+//
+//     [UIFormViewAction(Name = "Control Password", Order = 1, CustomComponent = typeof(PasswordUpdateComponent))]
+//     public   Action<int>? ControlePasswordAction { get; set; }
+}
