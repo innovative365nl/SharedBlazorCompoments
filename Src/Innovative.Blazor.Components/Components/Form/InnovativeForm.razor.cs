ba9e2a939b43920f468d34f0ba81b0c3b888317f
peereflits <peereflits@live.com>
2025-07-22 15:21:02 +0200

    Fix: Property of "data-test-id" cannot be found - error [#2572](https://github.com/innovative365nl/Automate/issues/2572)

--- Src/Innovative.Blazor.Components/Components/Form/InnovativeForm.razor.cs
+++ Src/Innovative.Blazor.Components/Components/Form/InnovativeForm.razor.cs
@@ -258,8 +258,7 @@ public partial class InnovativeForm<TModel> : ComponentBase, IFormComponent
             if (equalIndex > 0 && equalIndex < parameter.Length - 1)
             {
                 string paramName = parameter[..equalIndex];
-                equalIndex++;
-                string paramValue = parameter[equalIndex..];
+                string paramValue = parameter[++equalIndex..];
                 builder.AddAttribute(sequence: sequence++, name: paramName, value: Objectify(paramValue));
             }
         }
@@ -307,7 +306,7 @@ public partial class InnovativeForm<TModel> : ComponentBase, IFormComponent
                                                                                                             SetValue(propertyName: propName, value: val, attribute.ShouldNotifyChanges)));
 
                     if (!string.IsNullOrEmpty(attribute?.DataTestId))
-                        builder.AddAttribute(sequence++, DataTestIdKey, attribute.DataTestId);
+                        builder.AddAttribute(sequence++, nameof(attribute.DataTestId), attribute.DataTestId);
 
                     AppendFormParameters(builder, attribute?.FormParameters, ref sequence);
                     builder.CloseComponent();
