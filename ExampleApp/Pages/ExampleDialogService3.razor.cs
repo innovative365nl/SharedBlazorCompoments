3dc06fe144253977eeb6c7b56e696a492ac163db
Matthijs van der Plas <matthijs.vanderplas@innovative.nl>
2025-07-28 14:45:41 +0200

    Refactor and enhance multi-column dialog functionality: rename ThreeColumnModel to MultiColumnModel, add new columns and layouts, update dialog methods, and improve UI for dialog example.

--- ExampleApp/Pages/ExampleDialogService3.razor.cs
+++ ExampleApp/Pages/ExampleDialogService3.razor.cs
@@ -1,5 +1,7 @@
+using System.Reflection.PortableExecutable;
 using ExampleApp.Translations;
 using Innovative.Blazor.Components.Components;
+using Innovative.Blazor.Components.Enumerators;
 using Innovative.Blazor.Components.Services;
 using Microsoft.AspNetCore.Components;
 
@@ -9,7 +11,7 @@ public partial class ExampleDialogService3(IInnovativeSidePanelService sidePanel
 {
     private SingleColumnModel singleColumn = new();
     private DoubleColumnModel doubleColumn = new();
-    private ThreeColumnModel threeColumn = new();
+    private MultiColumnModel multiColumn = new();
 
     public async Task OpenOneColumnDialog()
     {
@@ -25,10 +27,24 @@ public partial class ExampleDialogService3(IInnovativeSidePanelService sidePanel
               .ConfigureAwait(false);
     }
 
-    public async Task OpenThreeColumnDialog()
+    public async Task OpenMultiColumnDialog()
     {
         await sidePanelService
-              .OpenInDisplayMode(threeColumn)
+              .OpenInDisplayMode(multiColumn)
+              .ConfigureAwait(false);
+    }
+
+    public async Task OpenMultiColumnDialogLarge()
+    {
+        await sidePanelService
+              .OpenInDisplayMode(multiColumn, width: SideDialogWidth.Large)
+              .ConfigureAwait(false);
+    }
+
+    public async Task OpenMultiColumnDialogExtraLarge()
+    {
+        await sidePanelService
+              .OpenInDisplayMode(multiColumn, width: SideDialogWidth.ExtraLarge)
               .ConfigureAwait(false);
     }
 }
@@ -73,27 +89,35 @@ public class DoubleColumnModel : FormModel
     public string Field4 { get; set; } = "Field4";
 }
 
-[UIFormClass(title: "Double Column Form", ResourceType = typeof(Example))]
-public class ThreeColumnModel : FormModel
+[UIFormClass(title: "Multi Column Form", ResourceType = typeof(Example))]
+public class MultiColumnModel : FormModel
 {
     private const string FirstColumn = "First";
     private const string SecondColumn = "Second";
     private const string ThirdColumn = "Third";
     private const string FullWidthColumn = "Fourth";
-    public ThreeColumnModel()
+    private const string BottomLeftColumn = "BottomLeft";
+    private const string BottomRightColumn = "BottomRight";
+    public MultiColumnModel()
     {
         AddViewColumn(name: FirstColumn, order: 1, width: 3, offset: 0);
         AddViewColumn(name: SecondColumn, order: 1, width: 3, offset: 0);
         AddViewColumn(name: ThirdColumn, order: 1, width: 6, offset: 0);
         AddViewColumn(name: FullWidthColumn, order: 1, width: 12, offset: 0);;
+        AddViewColumn(name: BottomLeftColumn, order: 1, width: 6, offset: 0);
+        AddViewColumn(name: BottomRightColumn, order: 1, width: 6, offset: 0);
     }
 
     [UIFormField(name: "FieldLeft1", ColumnGroup = FirstColumn)]
     public string Field1 { get; set; } = "Field1";
     [UIFormField(name: "FieldLeft2", ColumnGroup = SecondColumn)]
     public string Field2 { get; set; } = "Field2";
-    [UIFormField(name: "FieldRight3", ColumnGroup = ThirdColumn)]
+    [UIFormField(name: "FieldRight50%3", ColumnGroup = ThirdColumn, DisplayParameters = ["style=background-color: orange;"])]
     public string Field3 { get; set; } = "Field3";
-    [UIFormField(name: "FieldRight4", ColumnGroup = FullWidthColumn, FormParameters = ["style=background-color: pink;"], DisplayParameters = ["style=background-color: pink;"])]
+    [UIFormField(name: "FieldBottomFullWidth4", ColumnGroup = FullWidthColumn, FormParameters = ["style=background-color: pink;"], DisplayParameters = ["style=background-color: pink;"])]
     public string Field4 { get; set; } = "Field4";
+    [UIFormField("FieldInt50%width", ColumnGroup = BottomLeftColumn)]
+    public int Field5 { get; set; } = 5;
+    [UIFormField("FieldInt50%width", ColumnGroup = BottomRightColumn)]
+    public DateTime Field6 { get; set; } = DateTime.Today;
 }
